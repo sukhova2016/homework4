1c57bc010aa3982084b0d588df71f6d317030c19
Polina Drokina <sukhova2016@inbox.ru>
2022-11-03 14:03:02 +0400

    Добавлено решение третьей задачи

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt16(Console.ReadLine());
+Random r = new Random();
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+{
+    array[i] = r.Next(0, 100);
+    Console.Write(array[i] + " ");
+}
